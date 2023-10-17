1a034018b67aaf6a6d4eff545c53ae3228083a2f
JM-Xia <111189278+JM-Xia@users.noreply.github.com>
2023-10-18 01:57:38 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '7013c1ffae7547abbcc89ca2d62a20d9?v=4de05f1ee201495f8b2b7818145b61df&pvs=4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jiamin Xia',
+  domain: 'https://jiaminxia.notion.site/7013c1ffae7547abbcc89ca2d62a20d9?v=4de05f1ee201495f8b2b7818145b61df&pvs=4',
+  author: 'Jiamin',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Looking for Phd position 24Fall',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '@xia_jiamin75812',
+  github: ' ',
+  linkedin: 'https://www.linkedin.com/in/jiamin-xia-219208272/',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
